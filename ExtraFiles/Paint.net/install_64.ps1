4b4c2f8e3a890f0804e5ef56d36cd67556c44b8f
Stewart Bennell <stewart@bennell.me>
2021-11-24 08:01:03 +1100

    add Extra files for Paint.net

---
+++ ExtraFiles/Paint.net/install_64.ps1
@@ -0,0 +1,1 @@
+Start-Process msiexec.exe  -ArgumentList "/i `"PaintDotNet_x64.msi`" /qn /norestart DESKTOPSHORTCUT=`"0`" /L*V C:\Windows\Logs\PaintDotNet_x64.log" -Wait
---
+++ ExtraFiles/Paint.net/install_86.ps1
@@ -0,0 +1,1 @@
+Start-Process msiexec.exe  -ArgumentList "/i `"PaintDotNet_x86.msi`" /qn /norestart DESKTOPSHORTCUT=`"0`" /L*V C:\Windows\Logs\PaintDotNet_x64.log" -Wait
